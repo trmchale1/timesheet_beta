53c4ecfb4e4dcf9b293f51f67e302b36bbfa9d9f
trmchale1 <trmchale@gmail.com>
2014-06-26 05:27:47 -0400

    detail view controller with buttons

---
+++ timesheet_beta/V2thingsDetailVCTableViewController.h
@@ -0,0 +1,25 @@
+//
+//  V2thingsDetailVCTableViewController.h
+//  timesheet_beta
+//
+//  Created by Tim McHale on 6/26/14.
+//  Copyright (c) 2014 Tim McHale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class V2thingsDetailVCTableViewController;
+
+@protocol V2thingsDetailVCTableViewControllerDelegate <NSObject>
+- (void)V2thingsDetailVCTableViewControllerDidCancel:(V2thingsDetailVCTableViewController *)controller;
+- (void)V2thingsDetailVCTableViewControllerDidSave:(V2thingsDetailVCTableViewController *)controller;
+@end
+
+@interface  V2thingsDetailVCTableViewController : UITableViewController
+
+@property (nonatomic, weak) id <V2thingsDetailVCTableViewControllerDelegate> delegate;
+
+- (IBAction)cancel:(id)sender;
+- (IBAction)done:(id)sender;
+
+@end
